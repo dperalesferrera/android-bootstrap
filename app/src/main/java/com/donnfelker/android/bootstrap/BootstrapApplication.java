f0472005a8205a83b519b87bc24da68da862fc68
David Perales <dperales@iwhapp.com>
2016-10-16 15:49:12 +0200

    - Add AWS SNS feature - Upgrade dependencies, resolve conflicts and tarjetSdk

--- app/src/main/java/com/donnfelker/android/bootstrap/BootstrapApplication.java
+++ app/src/main/java/com/donnfelker/android/bootstrap/BootstrapApplication.java
@@ -3,7 +3,9 @@
 package com.donnfelker.android.bootstrap;
 
 import android.app.Application;
+import android.util.Log;
 import com.amazonaws.mobile.AWSMobileClient;
+import com.amazonaws.mobile.push.PushManager;
 
 /**
  * Android Bootstrap application
@@ -12,6 +14,7 @@ public abstract class BootstrapApplication extends Application {
 
     private static BootstrapApplication instance;
     private BootstrapComponent component;
+    private final static String LOG_TAG = Application.class.getSimpleName();
 
     /**
      * Create main application
@@ -43,6 +46,16 @@ public abstract class BootstrapApplication extends Application {
         AWSMobileClient.initializeMobileClientIfNecessary(getApplicationContext());
 
         // ... Put any application-specific initialization logic here ...
+
+        // Set a listener for changes in push notification state
+        PushManager.setPushStateListener(new PushManager.PushStateListener() {
+            @Override
+            public void onPushStateChange(final PushManager pushManager, boolean isEnabled) {
+                Log.d(LOG_TAG, "Push Notifications Enabled = " + isEnabled);
+                // ...Put any application-specific push state change logic here...
+            }
+        });
+
     }
 
 
